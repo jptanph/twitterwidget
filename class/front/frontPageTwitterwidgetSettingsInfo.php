db60e2268d2cb9c2bde922b7734f5ca693a92e3f
sdk_john <sdk_john@simplexi.com>
2012-01-30 17:19:30 +0800

    *las commit for 20123001

---
+++ class/front/frontPageTwitterwidgetSettingsInfo.php
@@ -0,0 +1,54 @@
+<?php
+require_once('builder/builderInterface.php');
+
+class frontPageTwitterwidgetSettingsInfo extends Controller_Front
+{
+    protected function run($aArgs)
+    {
+        $sAppId = $this->Request->getAppID();
+        $sImagePath = "/_sdk/img/$sAppId/";
+        $aData = array();
+        $model = new modelFront();
+
+        $aResult = $model->execGetUserInfo();
+        $sTwitterUrl = "http://api.twitter.com/1/statuses/user_timeline/".$aResult['username'].".xml?count=".$aResult['post_count']."";
+        $xTweetInfo = $this->twitterApi($sTwitterUrl);
+        $xData = simplexml_load_string($xTweetInfo);
+        $sHtml = '';
+        $sHtml .= "<input type='text' value='{$aResult['username']}'>";
+        $sHtml .= "<input type='text' value='{$aResult['post_count']}'>";
+        $sHtml .= "<input type='text' value='{$aResult['post_count']}'>";
+
+        $this->assign('sUsername',$sHtml);
+        $this->assign('iDbTotalTweet',$aResult['post_count']);
+        $this->assign('iTotalTweet',count($xData));
+
+    }
+
+    public function twitterApi($sUrl)
+    {
+        $ch = curl_init(); //initialize the CURL library.
+        curl_setopt($ch, CURLOPT_URL, $sUrl); // set the URL to post to.
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        return $result;
+    }
+
+    public function twitter_links($text)
+    {
+        # convert URLs into links
+        $text = preg_replace(
+                "#(https?://([-a-z0-9]+\.)+[a-z]{2,5}([/?][-a-z0-9!\#()/?&+]*)?)#i", "<a href='$1' target='_blank'>$1</a>",
+                $text);
+        # convert protocol-less URLs into links
+        $text = preg_replace(
+                "#(?!https?://|<a[^>]+>)(^|\s)(([-a-z0-9]+\.)+[a-z]{2,5}([/?][-a-z0-9!\#()/?&+.]*)?)\b#i", "$1<a href='http://$2'>$2</a>",
+                $text);
+
+        return $text;
+    }
+}
+
